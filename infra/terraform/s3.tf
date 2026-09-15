8dfc0a3368241e3469ad9acfed3b2794875a1680
mrdata355 <masteryourtracks718@gmail.com>
2026-09-15 14:06:29 -0400

    Upgrade S3 buckets to KMS encryption

--- infra/terraform/s3.tf
+++ infra/terraform/s3.tf
@@ -1,20 +1,21 @@
 resource "aws_s3_bucket" "data" {
   bucket = "oakbridge-${var.environment}-data-${var.aws_region}"
+  tags   = local.tags
 }
 
 resource "aws_s3_bucket_versioning" "data" {
   bucket = aws_s3_bucket.data.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+  versioning_configuration { status = "Enabled" }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
   bucket = aws_s3_bucket.data.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = aws_kms_key.data.arn
+      sse_algorithm     = "aws:kms"
     }
+    bucket_key_enabled = true
   }
 }
 
@@ -28,21 +29,22 @@ resource "aws_s3_bucket_public_access_block" "data" {
 
 resource "aws_s3_bucket" "stream_state" {
   bucket = "oakbridge-${var.environment}-stream-state-${var.aws_region}"
+  tags   = local.tags
 }
 
 resource "aws_s3_bucket_versioning" "stream_state" {
   bucket = aws_s3_bucket.stream_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+  versioning_configuration { status = "Enabled" }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "stream_state" {
   bucket = aws_s3_bucket.stream_state.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = aws_kms_key.data.arn
+      sse_algorithm     = "aws:kms"
     }
+    bucket_key_enabled = true
   }
 }
 
